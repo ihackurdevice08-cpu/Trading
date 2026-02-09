88a59ce54a23d8ae0f2fbc97ebf50b4b4f0cd9a2
ihackurdevice08-cpu <ihackurdevice08@gmail.com>
2026-02-10 00:48:26 +0900

    fix: pass children into PlasmicAppShell slot

--- components/AppShell.tsx
+++ components/AppShell.tsx
@@ -1,10 +1,8 @@
 "use client";
 
 import React from "react";
-import { PlasmicAppShell } from "./plasmic/man_cave/PlasmicAppShell";
+import PlasmicAppShell from "./plasmic/man_cave/PlasmicAppShell";
 
 export default function AppShell({ children }: { children: React.ReactNode }) {
-  const C: any = PlasmicAppShell;
-  // slot 이름이 children이든 content든 둘 중 하나는 먹게 "둘 다" 넣어둠
-  return <C children={children} content={children} />;
+  return <PlasmicAppShell children={children} />;
 }
